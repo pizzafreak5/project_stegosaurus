e2a3fd7fd28161295084dece8429033b9f2a0082
justinhockenberry <hockenberry.jb@gmail.com>
2016-11-13 01:08:54 -0700

    Refactoring complete 4 class structure: Main,View,Model,Controller

---
+++ Steganography/src/steganographyPackage/Controller.java
@@ -0,0 +1,303 @@
+package steganographyPackage;
+
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+public class Controller {
+	
+	private Model model;
+    private View view;
+    
+    
+    
+    
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+                          
+    }
+    
+    public void contol(){ 
+    	
+        view.lineButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.lineButton.isSelected())
+				{
+					view.hashButton.setSelected(false);
+					view.ttbButton.setSelected(false);
+					view.setEncryptMapIcon("linear");
+				}
+			}
+		});
+        view.hashButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.hashButton.isSelected())
+				{
+					view.lineButton.setSelected(false);
+					view.ttbButton.setSelected(false);
+					view.setEncryptMapIcon("hash");
+				}
+			}
+		});
+        view.ttbButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.ttbButton.isSelected())
+				{
+					view.lineButton.setSelected(false);
+					view.hashButton.setSelected(false);
+					view.setEncryptMapIcon("topdown");
+				}
+			}
+		});
+		
+        view.lineDecryptButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.lineDecryptButton.isSelected())
+				{
+					view.hashDecryptButton.setSelected(false);
+					view.ttbDecryptButton.setSelected(false);
+					view.setDecryptMapIcon("linear");
+				}
+			}
+		});
+        view.hashDecryptButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.hashDecryptButton.isSelected())
+				{
+					view.lineDecryptButton.setSelected(false);
+					view.ttbDecryptButton.setSelected(false);
+					view.setDecryptMapIcon("hash");
+				}
+			}
+		});
+        view.ttbDecryptButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.ttbDecryptButton.isSelected())
+				{
+					view.lineDecryptButton.setSelected(false);
+					view.hashDecryptButton.setSelected(false);
+					view.setDecryptMapIcon("topdown");
+				}
+			}
+		});
+		
+		
+        view.rButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.rButton.isSelected())
+				{
+					view.gButton.setSelected(false);
+					view.bButton.setSelected(false);
+					view.setColorIcon("red");
+				}
+			}
+		});
+        view.gButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.gButton.isSelected())
+				{
+					view.rButton.setSelected(false);
+					view.bButton.setSelected(false);
+					view.setColorIcon("green");
+				}
+			}
+		});
+        view.bButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(view.bButton.isSelected())
+				{
+					view.rButton.setSelected(false);
+					view.gButton.setSelected(false);
+					view.setColorIcon("blue");
+				}
+			}
+		});
+        
+        view.uploadButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				uploadEncryptFile();
+				checkIfReadyEncrypt();
+			}
+		});
+		view.encryptButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				encryptFile();
+				checkIfReadyEncrypt();
+			}
+		});
+		view.destinationButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				destinationFilePath();
+				checkIfReadyEncrypt();
+			}
+		});
+		
+		view.goButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				goEncrypt();
+			}
+		});
+		
+		 view.keyButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					keyToDecrypt();
+					checkIfReadyDecrypt();
+				}
+			});
+		 view.encryptedButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					fileToDecrypt();
+					checkIfReadyDecrypt();
+				}
+			});
+		view.goDecryptButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				goDecrypt();				
+			}
+		});
+		
+		
+		
+	}// end of control method
+        
+    private void checkIfReadyEncrypt(){
+    	
+    	if(model.getUplaodedImg() != null && 
+    			model.getImgToEncrypt() != null &&
+    			model.getDirectoryChooser() != null)
+    	{
+    		view.goButton.setBackground(Color.GREEN);
+    		view.goButton.setOpaque(true); 
+    		view.goButton.setBorderPainted(true);
+    		view.goButton.setEnabled(true);
+    	}
+    }
+    private void checkIfReadyDecrypt(){
+    	
+    	if(model.getOriginalImg() != null && 
+    			model.getImgToDecrypt() != null)
+    	{
+    		view.goDecryptButton.setBackground(Color.GREEN);
+    		view.goDecryptButton.setOpaque(true); 
+    		view.goDecryptButton.setBorderPainted(true);
+    		view.goDecryptButton.setEnabled(true);
+    	}
+    }
+    
+    private void goDecrypt(){
+    	model.goDecrypt();
+    	
+    	System.out.print("Mapping Function: ");
+		{
+			if(view.lineDecryptButton.isSelected())
+			{
+				System.out.println("Linear selected");
+			}
+			else if(view.hashDecryptButton.isSelected())
+			{
+				System.out.println("Hash selected");
+			}
+			else if(view.ttbDecryptButton.isSelected())
+			{
+				System.out.println("Top to Bottom selected");
+			}
+		}
+    	
+    }
+    private void goEncrypt() {
+		model.goEncrypt();	
+		
+		System.out.print("Color Plane: ");
+		if(view.rButton.isSelected())
+		{
+			System.out.println("Red selected");
+		}
+		else if(view.gButton.isSelected())
+		{
+			System.out.println("Green selected");
+		}
+		else if(view.bButton.isSelected())
+		{
+			System.out.println("Blue selected");
+		}
+		else
+		{
+			System.out.println("Unable to get selected color plane");
+		}
+		
+		System.out.print("Mapping Function: ");
+		{
+			if(view.lineButton.isSelected())
+			{
+				System.out.println("Linear selected");
+			}
+			else if(view.hashButton.isSelected())
+			{
+				System.out.println("Hash selected");
+			}
+			else if(view.ttbButton.isSelected())
+			{
+				System.out.println("Top to Bottom selected");
+			}
+		}
+		
+	}
+    private void uploadEncryptFile() {
+    	model.goUploadEncrypt(this.view);
+    	if(model.getUplaodedImg() != null)
+    	{
+    		view.uploadButton.setBackground(Color.GREEN);
+    		view.uploadButton.setOpaque(true); 
+    		view.uploadButton.setBorderPainted(true);
+    	}
+    	
+    	
+    }
+    private void encryptFile() {
+		model.encryptFile(this.view);
+		if(model.getImgToEncrypt() != null)
+    	{
+    		view.encryptButton.setBackground(Color.GREEN);
+    		view.encryptButton.setOpaque(true); 
+    		view.encryptButton.setBorderPainted(true);
+    	}
+		
+	}
+    private void destinationFilePath() {
+		model.destinationFilePath(this.view);
+		if(model.getDirectoryChooser() != null)
+    	{
+    		view.destinationButton.setBackground(Color.GREEN);
+    		view.destinationButton.setOpaque(true); 
+    		view.destinationButton.setBorderPainted(true);
+    	}
+	}
+    private void keyToDecrypt() {
+		model.keyToDecrypt(this.view);
+		if(model.getOriginalImg() != null)
+    	{
+    		view.keyButton.setBackground(Color.GREEN);
+    		view.keyButton.setOpaque(true); 
+    		view.keyButton.setBorderPainted(true);
+    	}
+		
+	}
+    private void fileToDecrypt() {
+		model.fileToDecrypt(this.view);
+		if(model.getImgToDecrypt() != null)
+    	{
+    		view.encryptedButton.setBackground(Color.GREEN);
+    		view.encryptedButton.setOpaque(true); 
+    		view.encryptedButton.setBorderPainted(true);
+    	}
+	}
+}
---
+++ Steganography/src/steganographyPackage/Model.java
@@ -0,0 +1,198 @@
+package steganographyPackage;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+
+import javax.swing.JLabel;
+
+public class Model {
+
+private int x;
+private BufferedImage uplaodedImg = null;
+private BufferedImage originalImg = null;
+private BufferedImage imgToEncrypt = null;
+private BufferedImage imgToDecrypt = null;
+
+private JFileChooser fileChooser = new JFileChooser();
+private JFileChooser directoryChooser = null;
+    
+    public Model(){
+        x = 0;
+    }
+    
+    public Model(int x){
+        this.x = x;
+    }
+    
+
+	public void goDecrypt() {
+		System.out.println("Application launch");
+	
+	}
+
+	public void goEncrypt() {
+		System.out.println("Application launch");
+		
+		
+	}
+
+	public void goUploadEncrypt(View view) {
+		
+		System.out.println("Upload picture was pressed");
+		
+			int returnVal = fileChooser.showOpenDialog(view.uploadButton);
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				
+				File file = fileChooser.getSelectedFile();
+				
+				// This is where a real application would open the file.
+				System.out.println("Opening: " + file.getName() + ".");
+				
+				
+				System.out.println("Reading image from disk. ");
+		        try {
+		        	
+		        	uplaodedImg = ImageIO.read(new File(file.getAbsolutePath()));
+					
+				} catch (IOException e1) {
+					System.out.println("Exception: " + e1.getMessage());
+				}
+			} else {
+				System.out.println("Open command cancelled by user.");
+			}
+		
+		
+	}
+	public void encryptFile(View view) {
+		System.out.println("Encrypt picture was pressed");
+		
+		
+			int returnVal = fileChooser.showOpenDialog(view.encryptButton);
+
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fileChooser.getSelectedFile();
+				
+				// This is where a real application would open the file.
+				System.out.println("Opening: " + file.getName() + ".");
+				
+				
+				System.out.println("Reading image from disk. ");
+		        try {
+		        	
+		        	imgToEncrypt = ImageIO.read(new File(file.getAbsolutePath()));
+					
+				} catch (IOException e1) {
+					System.out.println("Exception: " + e1.getMessage());
+				}
+			} else {
+				System.out.println("Open command cancelled by user.");
+			}
+		
+	}
+	public void destinationFilePath(View view) {
+		// TODO Auto-generated method stub
+		directoryChooser = new JFileChooser();
+		directoryChooser.setCurrentDirectory(new java.io.File("."));
+		directoryChooser.setDialogTitle("choosertitle");
+		directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		directoryChooser.setAcceptAllFileFilterUsed(false);
+
+		if (directoryChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			System.out.println("getCurrentDirectory(): " + directoryChooser.getCurrentDirectory());
+			System.out.println("getSelectedFile() : " + directoryChooser.getSelectedFile());
+		} else {
+			System.out.println("No Selection ");
+			directoryChooser = null;
+			view.destinationButton.setBackground(Color.RED);
+			view.destinationButton.setOpaque(true); 
+			view.destinationButton.setBorderPainted(true);
+		}
+
+	}
+	public void keyToDecrypt(View view) {
+		System.out.println("Key picture was pressed");
+		
+		
+		int returnVal = fileChooser.showOpenDialog(view.keyButton);
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+			
+			// This is where a real application would open the file.
+			System.out.println("Opening: " + file.getName() + ".");
+			
+			
+			System.out.println("Reading image from disk. ");
+	        try {
+	        	
+	        	originalImg = ImageIO.read(new File(file.getAbsolutePath()));
+				
+			} catch (IOException e1) {
+				System.out.println("Exception: " + e1.getMessage());
+			}
+		} else {
+			System.out.println("Open command cancelled by user.");
+		}
+		
+	}
+
+	public void fileToDecrypt(View view) {
+		System.out.println("Encrypted picture was pressed");
+		
+		
+		int returnVal = fileChooser.showOpenDialog(view.encryptedButton);
+
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+			
+			// This is where a real application would open the file.
+			System.out.println("Opening: " + file.getName() + ".");
+			
+			
+			System.out.println("Reading image from disk. ");
+	        try {
+	        	
+	        	imgToDecrypt = ImageIO.read(new File(file.getAbsolutePath()));
+				
+			} catch (IOException e1) {
+				System.out.println("Exception: " + e1.getMessage());
+			}
+		} else {
+			System.out.println("Open command cancelled by user.");
+		}
+		
+	}
+	public BufferedImage getUplaodedImg()
+	{
+		return this.uplaodedImg;
+	}
+	public BufferedImage getImgToEncrypt()
+	{ 
+		return this.imgToEncrypt;
+	}
+	
+	public BufferedImage getOriginalImg()
+	{
+		return this.originalImg;
+	}
+	public BufferedImage getImgToDecrypt()
+	{
+		return this.imgToDecrypt;
+	}
+	public JFileChooser getDirectoryChooser()
+	{
+		return this.directoryChooser;
+	}
+
+	
+
+	
+}
